c27309c63853f698e9c37b278d6fb91df27057e9
youssrifk <32481559+youssrifk@users.noreply.github.com>
2017-10-03 13:12:27 +0100

    Add files via upload

---
+++ tp2ex2.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : tp2ex2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+	float x;
+	printf("donner un reel");
+	scanf ("%f",&x);
+	if (x>0)
+	{
+		printf("%f",x);
+	}
+	else
+	{
+		x=x*-1;
+		printf("%f",x);
+	}-6
+}
